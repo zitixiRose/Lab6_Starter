e01da36560ead81fb042efec44a2b47668521e24
zitixiRose <zixiong@ucsd.edu>
2022-11-05 10:24:43 -0700

    expose

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,6 +24,9 @@ function getRecipesFromStorage() {
   // A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
+  var getRecipes = localStorage.getItem("recipes");
+  var recipes = JSON.parse(getRecipes);
+  return recipes;
 }
 
 /**
@@ -35,10 +38,16 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
   // A10. TODO - Get a reference to the <main> element
+  let mainEl = document.querySelector('main');
   // A11. TODO - Loop through each of the recipes in the passed in array,
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
+  for (let i in recipes) {
+    let card = document.createElement('recipe-card');
+    card.data = recipes[i];
+    mainEl.append(card);
+  }
 }
 
 /**
